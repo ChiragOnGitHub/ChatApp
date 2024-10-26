9892279bd302c7ab141857a52c6f4907c8d788d7
ChiragOnGitHub <chirag.cs102@gmail.com>
2024-10-27 02:47:48 +0530

    My signup Login Page

---
+++ server/middlewares/auth.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+//verifying user by cheking its JWT
+exports.auth = async (req, res, next) => {
+    try{
+        //extract token
+        const token = req.cookies.token 
+                        || req.body.token 
+                        || req.header("Authorization").replace("Bearer ", "");
+        console.log(token);
+
+        //checking if token is missing
+        if(!token) {
+            return res.status(401).json({
+                success:false,
+                message:'Token is missing',
+            });
+        }
+
+        //verifing the token
+        try{
+            const decodedPayload =  jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decodedPayload);
+            req.user = decodedPayload;
+        }
+        catch(err) {
+            //verification - issue
+            return res.status(401).json({
+                success:false,
+                message:'token is invalid',
+            });
+        }
+        next();
+    }
+    catch(error) {  
+        return res.status(401).json({
+            success:false,
+            message:'Something went wrong while validating the token',
+        });
+    }
+}
+
